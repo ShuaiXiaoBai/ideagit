2f95cfd22154e4b25ca295a6abc62d62bdee3a6f
ShuaiXiaoBai <a15908947665@gmail.com>
2020-07-11 17:37:04 +0800

    新增模块test1

---
+++ ideagittest/src/main/java/com/shuai/Test2.java
@@ -0,0 +1,13 @@
+package com.shuai;
+
+/**
+ * @author Shuai
+ * @create 2020-07-11 17:30
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("adsfasd");
+        System.out.println("adsfasd");
+        System.out.println("adsfasd");
+    }
+}
